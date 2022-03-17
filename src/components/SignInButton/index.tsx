5bb4d19cda3dd8fe01f4198328802070bc9b579c
marciocamello <marciocamello@oulook.com>
2022-03-17 19:55:22 +0000

    Add authentication with Github and next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,20 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
-
 export function SignInButton() {
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
+
+    const { data: session } = useSession();
+
+    return session ? (
         <button
             type='button'
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361" />
-            Marcio Camello
+            {session.user.name}
             <FiX
                 color="#737380"
                 className={styles.closeIcon}
@@ -20,6 +23,7 @@ export function SignInButton() {
     ) : (
         <button
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign in with Github
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 
+import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <SessionProvider session={pageProps.session}>
             <Header />
             <Component {...pageProps} />
-        </>
+        </SessionProvider>
     )
 }
 
